ed5ae2baba8057187e356651c8487a1b72c0501e
Andrius Simanaitis <eragonas5@gmail.com>
2020-08-30 21:23:33 +0300

    Updates

---
+++ precache-manifest.39c367c60cc25ea7adfb1d7fc78f5fae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a178779633805fa6d28b2dda574ebb21",
+    "url": "/BaltoSlavic/index.html"
+  },
+  {
+    "revision": "1cb01fcb7ebd8e61edce",
+    "url": "/BaltoSlavic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3889e583daa69774785",
+    "url": "/BaltoSlavic/static/js/2.9d023f18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BaltoSlavic/static/js/2.9d023f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb01fcb7ebd8e61edce",
+    "url": "/BaltoSlavic/static/js/main.ac425c75.chunk.js"
+  },
+  {
+    "revision": "7ff49ada5212fc96d579",
+    "url": "/BaltoSlavic/static/js/runtime-main.e66789bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BaltoSlavic/static/media/logo.5d5d9eef.svg"
+  }
+]);
